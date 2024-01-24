087550c0b085dce352be93f4ec838a6b7c175501
Fernando Nito <jfernando.nito@gmail.com>
2024-01-23 21:28:39 -0300

    Defining Component Props Types

--- react-ts-basics/src/components/CourseGoal.tsx
+++ react-ts-basics/src/components/CourseGoal.tsx
@@ -1,9 +1,15 @@
-export default function CourseGoal() {
+export default function CourseGoal({
+  title,
+  description,
+}: {
+  title: string
+  description: string
+}) {
   return (
     <article>
       <div>
-        <h2>TITLE</h2>
-        <p>DESCRIPTION</p>
+        <h2>{title}</h2>
+        <p>{description}</p>
       </div>
       <button>DELETE</button>
     </article>
