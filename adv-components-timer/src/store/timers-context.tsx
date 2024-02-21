70c6db9ea2351dd7aa7eae5d400f72414dd5e55a
Fernando Nito <jfernando.nito@gmail.com>
2024-02-20 22:37:41 -0300

    A Basic Reducer Function & A Basic Action Type

--- adv-components-timer/src/store/timers-context.tsx
+++ adv-components-timer/src/store/timers-context.tsx
@@ -37,22 +37,30 @@ type TimersContextProviderProps = {
   children: ReactNode
 }
 
+type Action = {
+  type: 'ADD_TIMER' | 'START_TIMERS' | 'STOP_TIMERS'
+}
+
+function timersReducer(state: TimersState, action: Action): TimersState {
+
+}
+
 export default function TimersContextProvider({
   children,
 }: TimersContextProviderProps) {
-  useReducer(reducer, initialState)
+  const [timersState, dispatch] = useReducer(timersReducer, initialState)
 
   const ctx: TimersContextValue = {
     timers: [],
     isRunning: true,
     addTimer: (timerData) => {
-      // ...
+      dispatch({ type: 'ADD_TIMER'})
     },
     startTimers: () => {
-      // ...
+      dispatch({ type: 'START_TIMERS'})
     },
     stopTimers: () => {
-      // ...
+      dispatch({ type: 'STOP_TIMERS'})
     },
   }
 
