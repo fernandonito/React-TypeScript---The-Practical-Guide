b5a259247404b33b11a380f42901053d43bc64d3
Fernando Nito <jfernando.nito@gmail.com>
2024-02-22 21:55:27 -0300

    Fetching & Transforming Data

--- data-fetching/src/App.tsx
+++ data-fetching/src/App.tsx
@@ -1,5 +1,51 @@
-function App() {
-  return <h1>Data Fetching!</h1>;
+import { ReactNode, useEffect, useState } from 'react'
+import { get } from './util/http'
+import BlogPosts, { type BlogPost } from './components/BlogPosts'
+import fetchingImg from './assets/data-fetching.png'
+
+type RawDataBlogPost = {
+  userId: number
+  id: number
+  title: string
+  body: string
 }
 
-export default App;
+function App() {
+  const [fetchedPosts, setFetchedPosts] = useState<BlogPost[]>()
+
+  useEffect(() => {
+    async function fetchPosts() {
+      const data = (await get(
+        'https://jsonplaceholder.typicode.com/posts'
+      )) as RawDataBlogPost[]
+
+      const blogPosts: BlogPost[] = data.map((rawPost) => ({
+        id: rawPost.id,
+        title: rawPost.title,
+        text: rawPost.body,
+      }))
+
+      setFetchedPosts(blogPosts)
+    }
+
+    fetchPosts()
+  }, [])
+
+  let content: ReactNode
+
+  if (fetchedPosts) {
+    content = <BlogPosts posts={fetchedPosts} />
+  }
+
+  return (
+    <main>
+      <img
+        src={fetchingImg}
+        alt="An abstract image depicting a data fetching process."
+      />
+      {content}
+    </main>
+  )
+}
+
+export default App
